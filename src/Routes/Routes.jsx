85c29a8e22ef779a6ae073f64942ee6e5d194099
Jashedul Islam Shaun <shauncuier@gmail.com>
2025-04-17 19:32:38 +0600

    Add Home component and enhance Navbar and Footer layout: implement responsive design and navigation links

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -5,6 +5,7 @@ import {
 } from "react-router";
 import Root from '../pages/Root/Root';
 import ErrorPage from '../pages/ErrorPage/ErrorPage';
+import Home from '../pages/Home/Home';
 
 
 
@@ -13,6 +14,14 @@ export const router = createBrowserRouter([
         path: "/",
         Component: Root,
         errorElement: <ErrorPage />,
-    },
+        children: [
+            {
+                index: true,
+                path: "/",
+                Component: Home,
+            },
+        ]
+    }
+
 ]);
 
