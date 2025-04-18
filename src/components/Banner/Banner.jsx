99cbf8b12efb733a74116ddd9a9c7efa814abc1d
Jashedul Islam Shaun <shauncuier@gmail.com>
2025-04-19 02:10:57 +0600

    Enhance Banner and Footer components: update styles for improved aesthetics and adjust layout in Home component

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -3,12 +3,12 @@ import BannerImg from '../../assets/book1.png';
 
 const Banner = () => {
     return (
-        <div className='flex justify-between items-center bg-gray-200'>
+        <div className='flex justify-between items-center bg-[#1313130D] p-32 rounded-3xl'>
             <div className='p-10'>
-                <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h1>
-                <button className='btn btn-primary'>Load More</button>
+                <h1 className='text-6xl font-bold text-[#131313]'>Books to freshen up <br /> your bookshelf</h1>
+                <button className='btn bg-[#23BE0A] text-[#FFFFFF] text-xl mt-12'>Load More</button>
             </div>
-            <div className='h-[668px]'>
+            <div className='h-[394px]'>
                 <img className='h-full' src={BannerImg} alt="" />
             </div>
         </div>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,8 +4,10 @@ import Banner from '../../components/Banner/Banner';
 const Home = () => {
     return (
         <div>
-            
             <Banner />
+            <h1 className='text-center my-10 text-5xl font-bold text-[#131313]'>Book</h1>
+
+            
         </div>
     );
 };
